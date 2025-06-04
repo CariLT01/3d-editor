2fad2cb89eb5c236ac935013d8d3be79e33adf74
= <publicuser0999847695359@gmail.com>
2025-06-04 18:07:07 -0400

    add loader

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,29 @@
+function hideLoadingScreen() {
+    const loadingScreen: HTMLDivElement = document.querySelector("#loading-screen") as HTMLDivElement;
+    if (!loadingScreen) {
+        console.warn("No loading screen found!");
+        return;
+    }
 
+    // Animate
+
+    loadingScreen.animate([
+        {opacity: 1},
+        {opacity: 0}
+    ], {
+        duration: 1000,
+        fill: 'forwards'
+    });
+
+    setTimeout(() => {
+        loadingScreen.style.display = "none";
+    }, 1000);
+}
 
 import { Editor3d } from './editor/editor';
 const editor = new Editor3d();
 editor.initialize();
+hideLoadingScreen()
 
 function renderLoop() {
     editor.renderScene();
