552255854ffc3fd04548af60959135cb566796ca
= <publicuser0999847695359@gmail.com>
2025-10-16 17:14:57 -0400

    rewrite progress 3

---
+++ src/ts/editor/EventBus.ts
@@ -0,0 +1,56 @@
+export enum EventType {
+
+}
+
+
+type EventCallback = (...args: any[] ) => any;
+
+export class EventBus {
+    private subscribedEvents: Map<EventType, EventCallback[]> = new Map();
+    private uniqueSubscribers: Map<string, EventCallback> = new Map(); // Events where only one can subscribe
+
+    constructor() {
+        
+        
+
+    }
+
+    subscribeEvent(eventType: EventType, callback: EventCallback) {
+        if (this.subscribedEvents.get(eventType) == null) {
+            this.subscribedEvents.set(eventType, []);
+        }
+
+        const eventSubscribed = this.subscribedEvents.get(eventType) as EventCallback[];
+        eventSubscribed.push(callback);
+    }
+
+
+    subscribeUniqueEvent(eventId: string, callback: EventCallback) {
+        if (this.uniqueSubscribers.get(eventId) != null) {
+            throw new Error("Event ID already has a subscriber");
+        }
+
+        this.uniqueSubscribers.set(eventId, callback);
+    }
+
+    postEvent(eventType: EventType, ...args: any[]) {
+        if (this.subscribedEvents.get(eventType) == null) {
+            return;
+        }
+        const eventSubscribed = this.subscribedEvents.get(eventType) as EventCallback[];
+
+        for (const callback of eventSubscribed) {
+            callback(...args);
+        }
+    }
+
+    inquireSubscriberUniqueEvent(eventId: string, ...args: any[]) {
+        if (this.uniqueSubscribers.get(eventId) == null) {
+            throw new Error("No subscriber");
+        }
+        const callback  =this.uniqueSubscribers.get(eventId);
+        if (callback == null) throw new Error("No callback");
+        return callback(...args);
+    }
+
+}
